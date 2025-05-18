e8c790c710bde29aaac2da32aadf47b592a02aa3
udonkot <udonko104develop@gmail.com>
2025-05-18 11:06:51 +0900

    ページ修正

--- src/main/java/jp/co/iglobe/dxservice/controller/DxserviceController.java
+++ src/main/java/jp/co/iglobe/dxservice/controller/DxserviceController.java
@@ -5,19 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class DxserviceController {
-	
+
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Spring Bootアプリケーションへようこそ！");
+        // model.addAttribute("message", "Spring Bootアプリケーションへようこそ！");
         return "index";
     }
-    
+
     @GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required=false, defaultValue="ゲスト") String name, Model model) {
-        model.addAttribute("name", name);
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "ゲスト") String name, Model model) {
+        // model.addAttribute("name", name);
         return "hello";
     }
 
